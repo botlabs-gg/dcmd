2e4562fb6b9216df2a4240bdf11cc57eb6222d86
jonas747 <jonasr747@gmail.com>
2018-12-20 03:19:05 +0100

    create a new UserError type to be able to filter out user errors from actual errors

--- errors.go
+++ errors.go
@@ -2,6 +2,7 @@ package dcmd
 
 import (
 	"fmt"
+	"github.com/pkg/errors"
 )
 
 type InvalidInt struct {
@@ -12,12 +13,20 @@ func (i *InvalidInt) Error() string {
 	return fmt.Sprintf("%q is not a whole number", i.Part)
 }
 
+func (i *InvalidInt) IsUserError() bool {
+	return true
+}
+
 type InvalidFloat struct {
 	Part string
 }
 
 func (i *InvalidFloat) Error() string {
 	return fmt.Sprintf("%q is not a number", i.Part)
+}
+
+func (i *InvalidFloat) IsUserError() bool {
+	return true
 }
 
 type ImproperMention struct {
@@ -28,6 +37,10 @@ func (i *ImproperMention) Error() string {
 	return fmt.Sprintf("Improper mention %q", i.Part)
 }
 
+func (i *ImproperMention) IsUserError() bool {
+	return true
+}
+
 type NoMention struct {
 	Part string
 }
@@ -36,12 +49,20 @@ func (i *NoMention) Error() string {
 	return fmt.Sprintf("No mention found in %q", i.Part)
 }
 
+func (i *NoMention) IsUserError() bool {
+	return true
+}
+
 type UserNotFound struct {
 	Part string
 }
 
 func (i *UserNotFound) Error() string {
 	return fmt.Sprintf("User %q not found", i.Part)
+}
+
+func (i *UserNotFound) IsUserError() bool {
+	return true
 }
 
 type OutOfRangeError struct {
@@ -73,4 +94,35 @@ func (o *OutOfRangeError) Error() string {
 	}
 
 	return fmt.Sprintf(intFormat, o.ArgName, preStr, o.Min, o.Max)
+}
+
+func (o *OutOfRangeError) IsUserError() bool {
+	return true
+}
+
+type UserError interface {
+	IsUserError() bool
+}
+
+func IsUserError(err error) bool {
+	v, ok := errors.Cause(err).(UserError)
+	if ok && v.IsUserError() {
+		return true
+	}
+
+	return false
+}
+
+type simpleUserError string
+
+func (s simpleUserError) Error() string {
+	return string(s)
+}
+
+func (s simpleUserError) IsUserError() bool {
+	return true
+}
+
+func NewSimpleUserError(args ...interface{}) error {
+	return simpleUserError(fmt.Sprint(args...))
 }
